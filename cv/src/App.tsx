94f883e8058cb9162371778830ba8ca0190314cb
giro39 <jan.wysmolinski@student.put.poznan.pl>
2025-02-18 13:49:40 +0100

    Add theme switching

--- cv/src/App.tsx
+++ cv/src/App.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 
 function App() {
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+
+    useEffect(() => {
+        document.documentElement.setAttribute("theme", theme);
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme(theme === "dark" ? "light" : "dark");
+    };
+
     return (
         <main>
-            <div>Initial commit</div>;
+            <div>Initial commit</div>
+            <button onClick={toggleTheme}>Zmień motyw</button>
         </main>
     );
 }
